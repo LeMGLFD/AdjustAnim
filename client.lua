eae78ae375a1dbc772750e4818d5ee9ffad6731e
LeM.Lua <160271648+LeMGLFD@users.noreply.github.com>
2024-08-23 16:45:46 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,17 +1,17 @@
 local function adjustAnim()
     local ped = PlayerPedId()
-    local getPlayerAnim = exports["rpemotes"]:IsPlayerInAnim()
-    if getPlayerAnim and not IsPedInAnyVehicle(ped, false) then
+    local getIfPlayerPlayAnim = exports["rpemotes"]:IsPlayerInAnim()
+    if getIfPlayerPlayAnim and not IsPedInAnyVehicle(ped, false) then
 
         local clonePed = ClonePed(ped, false, false, true)
-        local getAnim = exports["rpemotes"]:getPlayerAnim()
-        local animOption = getAnim.AnimationOptions
+        local getPlayedAnim = exports["rpemotes"]:getPlayerAnim()
+        local animOption = getPlayedAnim.AnimationOptions
         
-        if getAnim then
+        if getPlayedAnim then
             SetEntityNoCollisionEntity(ped, clonePed, false)
             FreezeEntityPosition(ped, true)
 
-            TaskPlayAnim(clonePed, getAnim[1], getAnim[2], 2.0, 2.0, -1, 0, 0, false, false, false)
+            TaskPlayAnim(clonePed, getPlayedAnim[1], getPlayedAnim[2], 2.0, 2.0, -1, 0, 0, false, false, false)
             SetEntityHeading(clonePed, GetEntityHeading(ped))
             SetEntityAlpha(clonePed, 50, true)
             FreezeEntityPosition(clonePed, true)
@@ -19,9 +19,9 @@ local function adjustAnim()
             TaskSetBlockingOfNonTemporaryEvents(clonePed, true)
 
             CreateThread(function()
-                while getPlayerAnim and clonePed do 
-                    if not IsEntityPlayingAnim(clonePed, getAnim[1], getAnim[2], 3) then
-                        TaskPlayAnim(clonePed, getAnim[1], getAnim[2], 5.0, 5.0, animOption.EmoteDuration, animOption.MovementType, 0, false, false, false)
+                while getIfPlayerPlayAnim and clonePed do 
+                    if not IsEntityPlayingAnim(clonePed, getPlayedAnim[1], getPlayedAnim[2], 3) then
+                        TaskPlayAnim(clonePed, getPlayedAnim[1], getPlayedAnim[2], 5.0, 5.0, animOption.EmoteDuration, animOption.MovementType, 0, false, false, false)
                     end
                     Wait(clonePed and 0 or 1000) 
                 end
@@ -32,12 +32,12 @@ local function adjustAnim()
             CreateThread(function()
                 while true do
                     local clonePedPose = GetEntityCoords(clonePed)
-                    local foundGround, groundZ = GetGroundZFor_3dCoord(clonePedPose.x, clonePedPose.y, clonePedPose.z, false)
-                    if foundGround then
+                    local retval, groundZ = GetGroundZFor_3dCoord(clonePedPose.x, clonePedPose.y, clonePedPose.z, false)
+                    if retval then
                         SetEntityCoordsNoOffset(ped, clonePedPose.x, clonePedPose.y, groundZ, false, false, false)
                         SetEntityHeading(ped, GetEntityHeading(clonePed))
                         DeleteEntity(clonePed)
-                        exports["rpemotes"]:EmoteCommandStart(getAnim[4])
+                        exports["rpemotes"]:EmoteCommandStart(getPlayedAnim[4])
                         FreezeEntityPosition(ped, false)
                         break
                     end
